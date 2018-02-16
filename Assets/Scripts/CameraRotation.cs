f91547830e953f19099cbda89352e9017bfb6932
OttoDan <33697887+OttoDan@users.noreply.github.com>
2018-02-16 23:40:20 +0100

    Added CameraRotation

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour {
+    public Camera cam;
+    float rotation = 0;
+    Vector3 origRelativePos;
+    Quaternion origRotation;
+    float rootRotation;
+    float maxRotation = 90;
+    float inputRotation;
+    float rotationSpeed = 2.5f;
+    float camDist = 24f;
+
+    void GetInput()
+    {
+        inputRotation = Input.GetAxis("RightJoystickHorizontal");
+    }
+	// Use this for initialization
+	void Start () {
+        origRelativePos = cam.transform.localPosition;
+        origRotation = cam.transform.localRotation;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        inputRotation = 0;
+        GetInput();
+        inputRotation *= rotationSpeed;
+        rotation += inputRotation;
+        //rotation = Mathf.Clamp(rotation, -maxRotation, maxRotation);
+        Debug.Log(rotation);
+        if (inputRotation != 0)
+        {
+            if (Mathf.Abs(rotation) > 90)
+                inputRotation = 0;
+            cam.transform.RotateAround(transform.position, Vector3.up, inputRotation);
+
+        }
+        else
+        {
+            cam.transform.localPosition = origRelativePos;
+            cam.transform.localRotation = origRotation;
+         //   cam.transform.RotateAround(transform.position, Vector3.up, transform.rotation.y - rotation);// * rotateBackSpeed);
+            rotation = 0;
+        }
+
+
+    }
+}
--- Assets/Scripts/CharacterControler.cs
+++ Assets/Scripts/CharacterControler.cs
@@ -7,14 +7,26 @@ public class CharacterControler : MonoBehaviour {
     public float inputDelay = 0.1f;
     public float forwardVel = 12;
     public float rotateVel = 100;
+    public float distToGrounded = 0.25f;
+    public float jumpVel = 8f;
+    public LayerMask ground;
+
+    //physic
+    public float downAccel = 0.25f;
+    Vector3 velocity = Vector3.zero;
 
     Quaternion targetRotation;
     Rigidbody rBody;
     float forwardInput, turnInput;
+    bool jumpInput;
 
     public Quaternion TargetRotation
     {
         get { return targetRotation; }
+    }
+    bool Grounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, distToGrounded, ground);
     }
     void Start()
     {
@@ -30,6 +42,7 @@ public class CharacterControler : MonoBehaviour {
     {
         forwardInput = Input.GetAxis("Vertical");
         turnInput = Input.GetAxis("Horizontal");
+        jumpInput = Input.GetButtonDown("Jump"); // non interpolated
     }
     void Update()
     {
@@ -38,20 +51,42 @@ public class CharacterControler : MonoBehaviour {
     }
     void FixedUpdate()
     {
-        Run();  
+        Run();
+        Jump();
+
+        rBody.velocity = transform.TransformDirection(velocity);
     }
     void Run()
     {
         if (Mathf.Abs(forwardInput) > inputDelay)
         {
-            rBody.velocity = transform.forward * forwardInput * forwardVel;
+            velocity.z = forwardVel * forwardInput;
         }
         else
-            rBody.velocity = Vector3.zero;
+            velocity = Vector3.zero;
     }
     void Trun()
     {
         targetRotation *= Quaternion.AngleAxis(rotateVel * turnInput * Time.deltaTime, Vector3.up);
         transform.rotation = targetRotation;
+    }
+    void Jump()
+    {
+        if (jumpInput && Grounded())
+        {
+            Debug.Log("jump!");
+            //jump
+            velocity.y = jumpVel;
+        }
+        else if (!jumpInput && Grounded())
+        {
+            // zero out our velocity.y
+            velocity.y = 0;
+        }
+        else //not grounded = falling
+        {
+            //decrease velocity.y
+            velocity.y -= downAccel;
+        }
     }
 }
